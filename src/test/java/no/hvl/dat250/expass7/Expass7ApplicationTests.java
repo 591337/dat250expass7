c5e4ac7a0c01be5344c8c1ffffa4dc071f012158
Martin <591337@stud.hvl.no>
2023-10-10 10:41:20 +0200

    start spring

---
+++ src/test/java/no/hvl/dat250/expass7/Expass7ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.expass7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expass7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
